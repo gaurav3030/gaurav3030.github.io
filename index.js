8c550633b7c67dbfed0b4015ed1c42a2bd858573
Gaurav Patil <gauravpatil8433@gmail.com>
2021-06-27 11:48:14 +0530

    background style

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 
+function myFunction() {
+    var elmnt = document.getElementById("myDIV");
+    var x = elmnt.scrollLeft;
+    var y = elmnt.scrollTop;
+
+    if(y<200){
+
+
+        document.getElementsByClassName("navlink")[0].classList.add("navactive");
+        document.getElementsByClassName("navlink")[1].classList.remove("navactive");
+
+    }
+    if(y>(screen.height-200)){
+
+
+        document.getElementsByClassName("navlink")[0].classList.remove("navactive");
+        document.getElementsByClassName("navlink")[1].classList.add("navactive");
+
+    }
+  }
